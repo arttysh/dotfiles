447a9aa4f50497832c40ca4e1638198a5db26bf6
Amit Raj <mail@amitraj.xyz>
2023-09-03 13:29:14 +0530

    nvim: added mappings for split resize and toggle dadbod-ui

--- config/nvim/lua/artty/remap.lua
+++ config/nvim/lua/artty/remap.lua
@@ -13,7 +13,7 @@ vim.keymap.set("n", "N", "Nzzzv")
 
 
 -- copy to clipboard selection
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 -- copy to clipboard entire line
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
@@ -24,10 +24,10 @@ vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
 -- easy navigations
-vim.keymap.set({"n", "i"}, "<C-j>", "<C-w><C-j>")
-vim.keymap.set({"n", "i"}, "<C-k>", "<C-w><C-k>")
-vim.keymap.set({"n", "i"}, "<C-h>", "<C-w><C-h>")
-vim.keymap.set({"n", "i"}, "<C-l>", "<C-w><C-l>")
+vim.keymap.set({ "n", "i" }, "<C-j>", "<C-w><C-j>")
+vim.keymap.set({ "n", "i" }, "<C-k>", "<C-w><C-k>")
+vim.keymap.set({ "n", "i" }, "<C-h>", "<C-w><C-h>")
+vim.keymap.set({ "n", "i" }, "<C-l>", "<C-w><C-l>")
 
 vim.keymap.set("n", "gj", ":tabprevious<CR>")
 vim.keymap.set("n", "gk", ":tabnext<CR>")
@@ -41,6 +41,21 @@ vim.keymap.set("n", "<C-p>", ":silent !tmux neww tms<CR>")
 -- vim fugitive
 vim.keymap.set("n", "gl", ":Git log<CR>")
 
+-- resize split
+vim.keymap.set("n", "<C-Up>", ":resize -2<CR>")
+vim.keymap.set("n", "<C-Down>", ":resize +2<CR>")
+vim.keymap.set("n", "<C-Left>", ":vertical resize -2<CR>")
+vim.keymap.set("n", "<C-Right>", ":vertical resize +2<CR>")
+
+-- resize split
+vim.keymap.set("n", "<C-S-Down>", ":resize +10<CR>")
+vim.keymap.set("n", "<C-S-Up>", ":resize -10<CR>")
+vim.keymap.set("n", "<C-S-Left>", ":vertical resize -10<CR>")
+vim.keymap.set("n", "<C-S-Right>", ":vertical resize +10<CR>")
+
+
+-- dadbod ui
+vim.keymap.set("n", "<leader>db", ":DBUIToggle<CR>")
 
 -- TODO:
 -- vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
