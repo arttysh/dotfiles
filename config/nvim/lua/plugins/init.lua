202d6625b63859e6b1538b5d61a0b53d14fec781
Amit Raj <mail@amitraj.xyz>
2025-05-11 23:58:54 +0530

    snipmate snippets nvim

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -194,6 +194,7 @@ return {
             local ls = require("luasnip")
             vim.keymap.set({ "i", "s" }, "<C-S>", function() ls.jump(1) end, { silent = true })
             vim.keymap.set({ "i", "s" }, "<C-A>", function() ls.jump(-1) end, { silent = true })
+            require("luasnip.loaders.from_snipmate").lazy_load({})
         end
     },
 
